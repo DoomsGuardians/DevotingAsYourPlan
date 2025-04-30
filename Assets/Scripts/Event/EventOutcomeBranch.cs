591a328c046667157217fab32a0e2a8c1dfbf335
DoomsGuardians <1753758397@qq.com>
2025-04-30 22:13:47 +0800

    #update

--- Assets/Scripts/Event/EventOutcomeBranch.cs
+++ Assets/Scripts/Event/EventOutcomeBranch.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 [CreateAssetMenu(menuName = "Events/分支")]
 public class EventOutcomeBranch : ScriptableObject
 {
